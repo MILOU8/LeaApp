47cab57fbad03d6dc89a89c62b5c9deb5424caf6
Mouad Anani <mouad.anani.1@ulaval.ca>
2014-10-14 10:33:37 -0400

    Initial Commit

---
+++ LeaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeaApp
+//
+//  Created by MouadMac on 2014-10-14.
+//  Copyright (c) 2014 co.fiye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeaApp
+//
+//  Created by MouadMac on 2014-10-14.
+//  Copyright (c) 2014 co.fiye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
